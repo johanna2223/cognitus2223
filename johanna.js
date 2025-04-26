87570309293bdc3d4755869d5d2fb0cc344616a2
johannap <johanna_ips@hotmail.com>
2025-04-26 12:45:47 -0500

    javascript

---
+++ johanna.js
@@ -0,0 +1,16 @@
+/*
+
+let johannap;
+
+johannap = 'hola perez';
+
+alert('Hoy decimos: ' + johannap);
+let nombre = 'Andres'
+alert(nombre);
+
+*/
+
+let name = prompt('Ingrese nombre');
+alert('El nombre que usted ha ingresado es: ' + name)
+
+console.log(name);
